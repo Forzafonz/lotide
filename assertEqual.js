906cdcee2df25857c04cb6911d1682a7100456a7
Anton Golubin <golubin1990@gmail.com>
2021-07-19 20:16:41 +0000

    Implementation of Assert Function

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+// Implementation of assert function.
+const flame = String.fromCodePoint(0x1F4A5);
+const good = String.fromCodePoint(0x1F929);
+
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log(good, good, good, `Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      console.log(flame, flame, flame, `Assertion Failed: ${actual} !== ${expected}`);
+    }
+};
