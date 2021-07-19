0d17fcc8a16f3dfdd6e503fcee1643a52be69885
Anton Golubin <golubin1990@gmail.com>
2021-07-19 20:18:05 +0000

    Linting

--- assertEqual.js
+++ assertEqual.js
@@ -3,9 +3,9 @@ const flame = String.fromCodePoint(0x1F4A5);
 const good = String.fromCodePoint(0x1F929);
 
 const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(good, good, good, `Assertion Passed: ${actual} === ${expected}`);
-    } else {
-      console.log(flame, flame, flame, `Assertion Failed: ${actual} !== ${expected}`);
-    }
+  if (actual === expected) {
+    console.log(good, good, good, `Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(flame, flame, flame, `Assertion Failed: ${actual} !== ${expected}`);
+  }
 };
