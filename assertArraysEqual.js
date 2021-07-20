360dd5b3a66ef34d52456514c7e1638607832e08
Anton <golubin1990@gmail.com>
2021-07-20 15:25:14 -0400

    Adding a function to compare two arrrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,23 @@
+const flame = String.fromCodePoint(0x1F4A5);
+const good = String.fromCodePoint(0x1F929);
+
+function eqArrays(array1, array2){
+  if (array1.length === array2.length){
+    for (var i =0; i < array1.length; i++){
+      if (array1[i] !== array2[i]){
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+}
+
+function assertArraysEqual(array1, array2){
+  if (eqArrays(array1,array2)) {
+    console.log(good, good, good, `Assertion Passed`);
+  } else {
+    console.log(flame, flame, flame, `Assertion Failed`);
+  }
+}
