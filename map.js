a9aff2ef9edb1c415dc27832492c190b3e1807ec
Anton <golubin1990@gmail.com>
2021-07-22 13:58:26 -0400

    Minor linting fixes

--- map.js
+++ map.js
@@ -3,15 +3,15 @@ const test = require('./assertArraysEqual');
 //Implementation of map function
 const map = function(array, callback) {
   let result = [];
-  for (let element of array){
+  for (let element of array) {
     result.push(callback(element));
   }
   return result;
-}
+};
 
 
 //Some Test cases
-if (require.main === module){
+if (require.main === module) {
   const words = ["ground", "control", "to", "major", "tom"];
   test.assertArraysEqual(map(words, word => word[0]), [ 'g', 'c', 't', 'm', 't' ]);
   const words2 = [];
