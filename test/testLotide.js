a0cdd84e4acd4bb317bd1c7c365553cc057bce37
Anton <golubin1990@gmail.com>
2021-07-26 15:44:52 -0400

    Added test cases for middle.js

--- test/testLotide.js
+++ test/testLotide.js
@@ -1,26 +1,39 @@
 const assert = require('chai').assert;
 const head   = require('../head');
 const tail   = require('../tail');
+const findMiddle = require('../middle');
+
 
 describe("#head", () => {
-  
-  it("returns 1 for [1, 2, 3]", () => {
+
+  it("should returns 1 for [1, 2, 3]", () => {
     assert.strictEqual(head([1, 2, 3]), 1);
   });
 
-  it("returns '5' for ['5']", () => {
+  it("should returns '5' for ['5']", () => {
     assert.strictEqual(head(['5']), '5'); 
   });
 });
 
 describe("#tail", () => {
 
-  it("return [2,3] for [1, 2, 3]", () => {
+  it("should return [2,3] for [1, 2, 3]", () => {
     assert.deepEqual(tail([1,2,3]), [2,3]);
   });
 
-  it("return [] for [1]", () => {
+  it("should return [] for [1]", () => {
     assert.deepEqual(tail([1]), []);
   });
-
 });
+
+  describe("#middle", () => {
+
+    it("should return [2,3] for [1, 2, 3]", () => {
+      assert.deepEqual(findMiddle([1, 2, 3, 4, 5, 6]), [3, 4]);
+    });
+
+    it("should return [] for [1]", () => {
+      assert.deepEqual(findMiddle([1]), []);
+    });
+
+  });
