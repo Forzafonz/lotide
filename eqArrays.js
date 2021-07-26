4680a0f6c45a5ce868431367ad1e5013df13167f
Anton <golubin1990@gmail.com>
2021-07-25 20:42:43 -0400

    Added recursion implementation for eqArrays function

--- eqArrays.js
+++ eqArrays.js
@@ -10,23 +10,24 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// function eqArrays(array1, array2) {
-//   if (array1.length === array2.length) {
-//     for (let i = 0; i < array1.length; i++) {
-//       if (array1[i] !== array2[i]) {
-//         return false;
-//       }
-//     }
-//   } else {
-//     return false;
-//   }
-//   return true;
-// }
-
 function eqArrays(array1, array2) {
-  if (array1.length === array2.length) {
-
+  if (array1.length === 0 && array2.length === 0) {
+    return true;
   }
+  if (array1.length === array2.length) {
+      if (Array.isArray(array1[0]) && Array.isArray(array2[0])){
+        if(!eqArrays(array1[0], array2[0]) || !eqArrays(array1.splice(1), array2.splice(1))){
+          return false;
+        };
+      } else if (array1[0] === array2[0]){
+        if(!eqArrays(array1.splice(1), array2.splice(1))) {
+         return false;
+        }
+      } else {
+        return false;
+        }
+  }
+  return true;
 }
 
 // Export
@@ -39,6 +40,10 @@ module.export = {
 
 if (require.main = module){
   console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])); // => true
-  console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])); // => false
-  console.log(qArrays([[2, 3], [4]], [[2, 3], 4])); // => false
+  console.log(eqArrays([[2, 3], [4]], [[2, 4], [4, 5]])); // => false
+  console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => false
+  console.log(eqArrays([1, 2, 3], [1, 2, 3]));// => true
+  console.log(eqArrays([1, 2, 3], [3, 2, 1]));// => false
+  console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+  console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
 }
