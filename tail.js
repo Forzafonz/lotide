74a399e3d2070b1968e8bea832c38081128afd08
Anton <golubin1990@gmail.com>
2021-07-26 14:24:20 -0400

    Refactored tails to remove assertEqual and added module export

--- tail.js
+++ tail.js
@@ -1,13 +1,6 @@
+const assertEqual = require("assertEqual.js");
 const flame = String.fromCodePoint(0x1F4A5);
 const good = String.fromCodePoint(0x1F929);
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(good, good, good, `Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(flame, flame, flame, `Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const tail = function(input) {
   if (Array.isArray(input) === false) {
@@ -21,3 +14,4 @@ const tail = function(input) {
   }
 };
 
+module.exports = tail;
