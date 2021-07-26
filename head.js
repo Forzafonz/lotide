6d1847763c0d2978969f393576a66c19d6b85a79
Anton <golubin1990@gmail.com>
2021-07-26 14:20:33 -0400

    Refactored the function to remove repetition and add export

--- head.js
+++ head.js
@@ -1,13 +1,6 @@
+const assertEqual = require('./assertEqual')
 const flame = String.fromCodePoint(0x1F4A5);
 const good = String.fromCodePoint(0x1F929);
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(good, good, good, `Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(flame, flame, flame, `Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const head = function(input) {
   if (Array.isArray(input) === false) {
@@ -20,3 +13,5 @@ const head = function(input) {
     }
   }
 };
+
+module.exports = head;
