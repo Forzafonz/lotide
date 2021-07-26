c10ffdb20578d956e7b3dc7663730aa734818831
Anton <golubin1990@gmail.com>
2021-07-26 16:51:14 -0400

    Fixing minor linting issues and adding export

--- flatten.js
+++ flatten.js
@@ -39,4 +39,11 @@ function flatten(array) {
   return array;
 }
 
-console.log(flatten([1, 2, [3, 4], 5, [6], [7, [8,9]]]));
+
+if (require.module === main){
+  
+  console.log(flatten([1, 2, [3, 4], 5, [6], [7, [8,9]]]));
+
+}
+
+module.exports = flatten;
