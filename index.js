6933f5d44905d1fda1a9f785509bcd2a62a5ecb4
Anton <golubin1990@gmail.com>
2021-07-26 16:54:04 -0400

    Added functions takeUntil, map, flatten, countOnly to index file

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
+const takeUntil = require('./takeUntil');
+const map = require('./map.js');
+const flatten = require('./flatten.js');
+const countOnly = require('./countOnly.js')
 
 module.exports = {
   head,
   tail,
   middle,
+  takeUntil,
+  map,
+  flatten,
+  countOnly
 };
