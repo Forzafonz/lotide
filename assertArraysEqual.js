aafd5c9bcad1de1f08eca62717ba515b9b00c22c
Anton <golubin1990@gmail.com>
2021-07-26 14:32:50 -0400

    Refactored code and added export feature

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,6 @@
-
+const eqArrays = require('eqArrays');
 const flame = String.fromCodePoint(0x1F4A5);
 const good = String.fromCodePoint(0x1F929);
-
-function eqArrays(array1, array2) {
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-  } else {
-    return false;
-  }
-  return true;
-}
 
 function assertArraysEqual(array1, array2) {
   if (eqArrays(array1,array2)) {
@@ -23,9 +10,8 @@ function assertArraysEqual(array1, array2) {
   }
 }
 
-module.exports = {
-  assertArraysEqual
-};
+module.exports = assertArraysEqual;
+
 
 if (require.main === module) {
   assertArraysEqual([1,2], [2,3]);
